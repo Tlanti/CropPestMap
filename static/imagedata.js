faef01e0633442bab726b7eecd81032e560576af
Melnik8 <maria8melnikova@gmail.com>
2015-04-11 21:56:48 +0400

    Adds
    adds images file

---
+++ static/imagedata.js
@@ -0,0 +1,147 @@
+var imagesData = [
+  {
+    "name":"Brown rot of potato (lat. Ralstonia solanacearum)",
+    "eppocode":"RALSSO",
+    "images":[
+      {
+        "imageName":"http://www.inspection.gc.ca/DAM/DAM-plants-vegetaux/STAGING/images-images/potatoe_guidance_pi009_app3_image2_1383848295491_eng.jpg"
+      },
+      {
+        "imageName":"http://bugwoodcloud.org/images/768x512/1235088.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Colorado Beetle (lat. Leptinotarsa decemlineata)",
+    "eppocode":"LPTNDE",
+    "images":[
+      {
+        "imageName":"http://www.biol.uni.wroc.pl/cassidae/European%20Chrysomelidae/Iconography/Leptinotarsa%20decemlineata.jpg"
+      },
+      {
+        "imageName":"http://ak.picdn.net/shutterstock/videos/1321132/preview/stock-footage-colorado-beetle-larva-leptinotarsa-decemlineata-agriculture-pest-timelapse.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Potato cyst nematodes (lat. Globodera rostochiensis)",
+    "eppocode":"HETDRO",
+    "images":[
+      {
+        "imageName":"http://www.elitkartofel.com/wp-content/uploads/2012/10/b4787a4a5d6711adf08fa27fc1cba139_XL.jpg"
+      },
+      {
+        "imageName":"http://www.agroatlas.ru/content/pests/Globodera_rostochiensis/Globodera_rostochiensis.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Potato wart disease (lat. Synchytrium endobioticum)",
+    "eppocode":"SYNCEN",
+    "images":[
+      {
+        "imageName":"http://www.maff.go.jp/pps/j/introduction/gallery/img/insect-jagaimoganshu.jpg"
+      },
+      {
+        "imageName":"http://agrifish.dk/fileadmin/user_upload/NaturErhverv/Billeder/Skadegorer/Svampe/SYNCEN_05.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Beet necrotic yellow vein virus",
+    "eppocode":"BNYVV0",
+    "images":[
+      {
+        "imageName":"http://www.eppo.int/QUARANTINE/virus/Beet_necrotic_yellow_vein_virus/BNYVV0_02.jpg"
+      },
+      {
+        "imageName":"http://www7.inra.fr/hyp3/images/6034254.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Ring rot of potato (lat. Corynebacterium sepedonicum)",
+    "eppocode":"CORBSE",
+    "images":[
+      {
+        "imageName":"http://kartofel.at.ua/_pu/0/68479595.jpg"
+      },
+      {
+        "imageName":"http://agrifish.dk/fileadmin/user_upload/NaturErhverv/Billeder/Skadegorer/Bakterier/Clavibacter%20mich%20sep.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Angular leaf spot of strawberry Asian longhorn beetle (lat. Xanthomonas fragariae)",
+    "eppocode":"XANTFR",
+    "images":[
+      {
+        "imageName":"http://media.padil.gov.au/species/136653/4665-medium.jpg"
+      },
+      {
+        "imageName":"https://perambulations.files.wordpress.com/2011/04/asianlonghorned071.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Citrus longhorn beetle (lat. Anoplophora malasiaca)",
+    "eppocode":"ANOLCN",
+    "images":[
+      {
+        "imageName":"http://www.unipd.it/esterni/wwwfitfo/immagini/anoplophora-adultoegallerie.jpg"
+      },
+      {
+        "imageName":"https://www.nvwa.nl/txmpub/images/2000260/404/large/22541.jpg"
+      }
+    ]
+  },
+  {
+    "name":"Fireblight (Erwinia amylovora)",
+    "eppocode":"ERWIAM",
+    "images":[
+      {
+        "imageName":"http://www.fomesa.net/Calidad/Variedades/Enfermedades/Pepita/Img/Erwinia%20amylovora%20-%20Fuego%20bacteriano%2015.jpg"
+      },
+      {
+        "imageName":"http://www.agromatica.es/wp-content/uploads/2012/10/fuego-bacteriano.png"
+      }
+    ]
+  },
+  {
+    "name":" ()",
+    "eppocode":"",
+    "images":[
+      {
+        "imageName":"http://example.com"
+      },
+      {
+        "imageName":"http://example.com"
+      }
+    ]
+  },
+  {
+    "name":" ()",
+    "eppocode":"",
+    "images":[
+      {
+        "imageName":"http://example.com"
+      },
+      {
+        "imageName":"http://example.com"
+      }
+    ]
+  },
+  {
+    "name":" ()",
+    "eppocode":"",
+    "images":[
+      {
+        "imageName":"http://example.com"
+      },
+      {
+        "imageName":"http://example.com"
+      }
+    ]
+  },
+
+]
